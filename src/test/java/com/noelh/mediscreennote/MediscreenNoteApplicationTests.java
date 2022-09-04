8497c6d60f888b9b9b179c3db7c7c5a14d82d321
Noel-H <noel.huberto@gmail.com>
2022-09-04 18:50:16 +0200

    Initial commit

---
+++ src/test/java/com/noelh/mediscreennote/MediscreenNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelh.mediscreennote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
